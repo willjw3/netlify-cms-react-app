89ebb3babba4a3696b507ce54c057070b220d018
willjw3 <will.j.ward3@gmail.com>
2020-01-05 14:13:36 +0900

    Switched to Browser Router and Added new Post

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route} from "react-router-dom"
+import { BrowserRouter as Router, Route} from "react-router-dom"
 import './index.css';
 import "typeface-raleway"
 import Home from "./pages/home"
